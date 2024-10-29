a7b6da139750fec13cfd48d3928029715ef5ad92
Abdullah Salman <abdullah.coolabs@gmail.com>
2024-10-28 22:52:33 -0400

    Update knight class to new ChessPiece definition

--- src/Knight.cpp
+++ src/Knight.cpp
@@ -5,25 +5,30 @@ Knight::~Knight(){};
 
 string Knight::getIdentifier() const { return "K"; }
 
-bool Knight::isValidMove(Board &board, int fromIndex, int toIndex) { 
-  int rowDiff = abs(board.getRow(fromIndex) - board.getRow(toIndex));
-  int colDdiff = abs(board.getCol(fromIndex) - board.getCol(toIndex));
-  if (rowDiff == 2 && colDdiff == 1)
+bool Knight::isValidMove(Board &board, int fromRow, int fromCol, int toRow, int toCol) { 
+  int rowDiff = abs(fromRow - toRow);
+  int colDiff = abs(fromCol - toCol);
+  if (rowDiff == 2 && colDiff == 1)
     return true;
-  if (rowDiff == 1 && colDdiff == 2)
+  if (rowDiff == 1 && colDiff == 2)
     return true;
   return false;
 };
 
-unordered_set<int> Knight::getAllValidMoves(Board &board, int index) {
+unordered_set<int> Knight::getAllValidMoves(Board &board, int row, int col) {
   std::unordered_set<int> validMoves;
-  int row = board.getRow(index);
-  int col = board.getCol(index);
   int rowOffset[] = {2, 1, -1, -2, -2, -1, 1, 2};
   int colOffset[] = {1, 2, 2, 1, -1, -2, -2, -1};
   for (int i = 0; i < 8; i++) 
-    if (board.isOnBoard(row + rowOffset[i], col + colOffset[i]))
-        validMoves.insert(board.getIndex(row + rowOffset[i], col + colOffset[i]));
-  
+  {
+    int newRow = row + rowOffset[i];
+    int newCol = col + colOffset[i];
+    if (!board.isOnBoard(newRow, newCol))
+      continue;
+
+    ChessPiece *currentPiece = board.getPiece(newRow, newCol);
+    if (currentPiece == nullptr || currentPiece->isSameTeam(this->color))
+        validMoves.insert(board.getIndex(newRow, newCol));
+  }
   return validMoves;
 }
