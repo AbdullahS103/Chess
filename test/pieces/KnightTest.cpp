4905480ac68d725901de0e88dbc545276dddaa0f
Abdullah Salman <abdullah.coolabs@gmail.com>
2024-11-15 23:12:15 -0500

    Add unit tests for Knight and Rook

---
+++ test/pieces/KnightTest.cpp
@@ -0,0 +1,118 @@
+#include <gtest/gtest.h>
+#include "Knight.h"
+#include "Board.h"
+
+// Knight should be able to move lol
+TEST(KnightTests, ValidMoveTest1) {
+  Board testBoard = Board();
+  testBoard.setPiece(2, 2, new Knight(TeamColors::WHITE));
+  EXPECT_TRUE(testBoard.getPiece(2, 2)->isValidMove(testBoard, 2, 2, 0, 1));
+  EXPECT_TRUE(testBoard.getPiece(2, 2)->isValidMove(testBoard, 2, 2, 0, 3));
+  EXPECT_TRUE(testBoard.getPiece(2, 2)->isValidMove(testBoard, 2, 2, 1, 0));
+  EXPECT_TRUE(testBoard.getPiece(2, 2)->isValidMove(testBoard, 2, 2, 1, 4));
+  EXPECT_TRUE(testBoard.getPiece(2, 2)->isValidMove(testBoard, 2, 2, 3, 0));
+  EXPECT_TRUE(testBoard.getPiece(2, 2)->isValidMove(testBoard, 2, 2, 3, 4));
+  EXPECT_TRUE(testBoard.getPiece(2, 2)->isValidMove(testBoard, 2, 2, 4, 1));
+  EXPECT_TRUE(testBoard.getPiece(2, 2)->isValidMove(testBoard, 2, 2, 4, 3));
+}
+
+// Knight should not move to a space already occupied by piece of same team
+TEST(KnightTests, CollisionTest1) {
+  Board testBoard = Board();
+  testBoard.setPiece(3, 4, new Knight(TeamColors::WHITE));
+  testBoard.setPiece(2, 2, new Knight(TeamColors::WHITE));
+  EXPECT_FALSE(testBoard.getPiece(3, 4)->isValidMove(testBoard, 3, 4, 2, 2));
+}
+
+// Knight should be able to move to a space already occupied by piece of opposing team
+TEST(KnightTests, CollisionTest2) {
+  Board testBoard = Board();
+  testBoard.setPiece(3, 4, new Knight(TeamColors::BLACK));
+  testBoard.setPiece(2, 2, new Knight(TeamColors::WHITE));
+  EXPECT_TRUE(testBoard.getPiece(3, 4)->isValidMove(testBoard, 3, 4, 2, 2));
+}
+
+// Knight cannot move to the square it is already in
+TEST(KnightTests, InvalidMoveTest1) {
+  Board testBoard = Board();
+  testBoard.setPiece(3, 4, new Knight(TeamColors::WHITE));
+  EXPECT_FALSE(testBoard.getPiece(3, 4)->isValidMove(testBoard, 3, 4, 3, 4));
+}
+
+// Knight should not be able to move in a non-L fashion
+TEST(KnightTests, InvalidMoveTest2) {
+  Board testBoard = Board();
+  testBoard.setPiece(3, 4, new Knight(TeamColors::WHITE));
+  EXPECT_FALSE(testBoard.getPiece(3, 4)->isValidMove(testBoard, 3, 4, 3, 5));
+  EXPECT_FALSE(testBoard.getPiece(3, 4)->isValidMove(testBoard, 3, 4, 5, 4));
+  EXPECT_FALSE(testBoard.getPiece(3, 4)->isValidMove(testBoard, 3, 4, 2, 5));
+  EXPECT_FALSE(testBoard.getPiece(3, 4)->isValidMove(testBoard, 3, 4, 7, 7));
+}
+
+// Knight should not be able to move out of bounds
+TEST(KnightTests, OutOfBoundsCheck1) {
+  Board testBoard = Board();
+  testBoard.setPiece(0, 0, new Knight(TeamColors::WHITE));
+  EXPECT_FALSE(testBoard.getPiece(0, 0)->isValidMove(testBoard, 0, 0, -1, -2));
+  EXPECT_FALSE(testBoard.getPiece(0, 0)->isValidMove(testBoard, 0, 0, -2, -1));
+}
+
+TEST(KnightTests, OutOfBoundsCheck2) {
+  Board testBoard = Board();
+  testBoard.setPiece(7, 7, new Knight(TeamColors::WHITE));
+  EXPECT_FALSE(testBoard.getPiece(7, 7)->isValidMove(testBoard, 7, 7, 8, 9));
+  EXPECT_FALSE(testBoard.getPiece(7, 7)->isValidMove(testBoard, 7, 7, 9, 8));
+}
+
+// Knight's valid moves when in center of board and multiple pieces around it
+TEST(KnightTests, AllValidMovesTest1) {
+  Board testBoard = Board();
+  testBoard.setPiece(2, 2, new Knight(TeamColors::WHITE));
+  testBoard.setPiece(0, 1, new Knight(TeamColors::WHITE));
+  testBoard.setPiece(1, 4, new Knight(TeamColors::BLACK));
+  testBoard.setPiece(4, 3, new Knight(TeamColors::BLACK));
+  testBoard.setPiece(3, 4, new Knight(TeamColors::WHITE));
+
+  std::unordered_set<int> moves = testBoard.getPiece(2, 2)->getAllValidMoves(testBoard, 2, 2);
+  std::unordered_set<int> answer;
+  answer.insert(testBoard.getIndex(0, 3));
+  answer.insert(testBoard.getIndex(1, 0));
+  answer.insert(testBoard.getIndex(1, 4));
+  answer.insert(testBoard.getIndex(3, 0));
+  answer.insert(testBoard.getIndex(4, 1));
+  answer.insert(testBoard.getIndex(4, 3));
+
+  EXPECT_TRUE(answer == moves);
+}
+
+// Knight's valid moves when in the corner
+TEST(KnightTests, AllValidMovesTest2) {
+  Board testBoard = Board();
+  testBoard.setPiece(0, 0, new Knight(TeamColors::WHITE));
+
+  std::unordered_set<int> moves = testBoard.getPiece(0, 0)->getAllValidMoves(testBoard, 0, 0);
+  std::unordered_set<int> answer;
+  answer.insert(testBoard.getIndex(2, 1));
+  answer.insert(testBoard.getIndex(1, 2));
+
+  EXPECT_TRUE(answer == moves);
+}
+
+TEST(KnightTests, OperatorTest1) {
+  Knight knight1 = Knight(TeamColors::WHITE);
+  Knight knight2 = Knight(TeamColors::WHITE);
+  Knight knight3 = Knight(TeamColors::BLACK);
+
+  EXPECT_TRUE(knight1 == knight1);
+  EXPECT_TRUE(knight1 == knight2);
+  EXPECT_TRUE(knight2 != knight3);
+  EXPECT_FALSE(knight2 == knight3);
+}
+
+TEST(KnightTests, OperatorTest2) {
+  Knight knight = Knight(TeamColors::WHITE);
+  ChessPiece piece = ChessPiece(TeamColors::WHITE);
+
+  EXPECT_TRUE(knight != piece);
+  EXPECT_FALSE(knight == piece);
+}
---
+++ test/pieces/RookTest.cpp
@@ -0,0 +1,154 @@
+#include <gtest/gtest.h>
+#include "Rook.h"
+#include "Board.h"
+
+// Rook can move horizontally
+TEST(RookTests, ValidMoveTest1) {
+  Board testBoard = Board();
+  testBoard.setPiece(3, 4, new Rook(TeamColors::WHITE));
+  EXPECT_TRUE(testBoard.getPiece(3, 4)->isValidMove(testBoard, 3, 4, 3, 5));
+  EXPECT_TRUE(testBoard.getPiece(3, 4)->isValidMove(testBoard, 3, 4, 3, 6));
+  EXPECT_TRUE(testBoard.getPiece(3, 4)->isValidMove(testBoard, 3, 4, 3, 2));
+  EXPECT_TRUE(testBoard.getPiece(3, 4)->isValidMove(testBoard, 3, 4, 3, 0));
+}
+
+// Rook can move vertically
+TEST(RookTests, ValidMoveTest2) {
+  Board testBoard = Board();
+  testBoard.setPiece(3, 4, new Rook(TeamColors::WHITE));
+  EXPECT_TRUE(testBoard.getPiece(3, 4)->isValidMove(testBoard, 3, 4, 7, 4));
+  EXPECT_TRUE(testBoard.getPiece(3, 4)->isValidMove(testBoard, 3, 4, 6, 4));
+  EXPECT_TRUE(testBoard.getPiece(3, 4)->isValidMove(testBoard, 3, 4, 2, 4));
+  EXPECT_TRUE(testBoard.getPiece(3, 4)->isValidMove(testBoard, 3, 4, 1, 4));
+}
+
+// Rook should not move to a space already occupied by piece of same team
+TEST(RookTests, CollisionTest1) {
+  Board testBoard = Board();
+  testBoard.setPiece(3, 4, new Rook(TeamColors::WHITE));
+  testBoard.setPiece(3, 3, new Rook(TeamColors::WHITE));
+  EXPECT_FALSE(testBoard.getPiece(3, 4)->isValidMove(testBoard, 3, 4, 3, 3));
+}
+
+// Rook should be able to move to a space already occupied by piece of opposing team
+TEST(RookTests, CollisionTest2) {
+  Board testBoard = Board();
+  testBoard.setPiece(3, 4, new Rook(TeamColors::BLACK));
+  testBoard.setPiece(3, 3, new Rook(TeamColors::WHITE));
+  EXPECT_TRUE(testBoard.getPiece(3, 4)->isValidMove(testBoard, 3, 4, 3, 3));
+}
+
+// Rook cannot move to the square it is already in
+TEST(RookTests, InvalidMoveTest1) {
+  Board testBoard = Board();
+  testBoard.setPiece(3, 4, new Rook(TeamColors::WHITE));
+  EXPECT_FALSE(testBoard.getPiece(3, 4)->isValidMove(testBoard, 3, 4, 3, 4));
+}
+
+// Rook should not be able to move more than 1 square diagonally/anywhere else
+TEST(RookTests, InvalidMoveTest2) {
+  Board testBoard = Board();
+  testBoard.setPiece(3, 4, new Rook(TeamColors::WHITE));
+  EXPECT_FALSE(testBoard.getPiece(3, 4)->isValidMove(testBoard, 3, 4, 0, 0));
+  EXPECT_FALSE(testBoard.getPiece(3, 4)->isValidMove(testBoard, 3, 4, 4, 8));
+  EXPECT_FALSE(testBoard.getPiece(3, 4)->isValidMove(testBoard, 3, 4, 4, 5));
+  EXPECT_FALSE(testBoard.getPiece(3, 4)->isValidMove(testBoard, 3, 4, 2, 3));
+  EXPECT_FALSE(testBoard.getPiece(3, 4)->isValidMove(testBoard, 3, 4, 7, 7));
+}
+
+// Rook should not be able go past another piece horizontally/vertically
+TEST(RookTests, InvalidMoveTest3) {
+  Board testBoard = Board();
+  testBoard.setPiece(3, 4, new Rook(TeamColors::WHITE));
+  testBoard.setPiece(3, 6, new Rook(TeamColors::BLACK));
+  testBoard.setPiece(1, 4, new Rook(TeamColors::BLACK));
+
+  EXPECT_TRUE(testBoard.getPiece(3, 4)->isValidMove(testBoard, 3, 4, 2, 4));
+  EXPECT_FALSE(testBoard.getPiece(3, 4)->isValidMove(testBoard, 3, 4, 3, 7));
+  EXPECT_FALSE(testBoard.getPiece(3, 4)->isValidMove(testBoard, 3, 4, 0, 4));
+}
+
+
+// Rook should not be able to move out of bounds
+TEST(RookTests, OutOfBoundsCheck1) {
+  Board testBoard = Board();
+  testBoard.setPiece(0, 0, new Rook(TeamColors::WHITE));
+  EXPECT_FALSE(testBoard.getPiece(0, 0)->isValidMove(testBoard, 0, 0, -5, 0));
+  EXPECT_FALSE(testBoard.getPiece(0, 0)->isValidMove(testBoard, 0, 0, 0, -1));
+}
+
+TEST(RookTests, OutOfBoundsCheck2) {
+  Board testBoard = Board();
+  testBoard.setPiece(7, 7, new Rook(TeamColors::WHITE));
+  EXPECT_FALSE(testBoard.getPiece(7, 7)->isValidMove(testBoard, 7, 7, 9, 7));
+  EXPECT_FALSE(testBoard.getPiece(7, 7)->isValidMove(testBoard, 7, 7, 7, 10));
+}
+
+// Rooks's valid moves when in center of board and multiple pieces around it
+TEST(RookTests, AllValidMovesTest1) {
+  Board testBoard = Board();
+  testBoard.setPiece(5, 5, new Rook(TeamColors::WHITE));
+  testBoard.setPiece(5, 3, new Rook(TeamColors::WHITE));
+  testBoard.setPiece(1, 5, new Rook(TeamColors::BLACK));
+  testBoard.setPiece(5, 7, new Rook(TeamColors::BLACK));
+
+  std::unordered_set<int> moves = testBoard.getPiece(5, 5)->getAllValidMoves(testBoard, 5, 5);
+  std::unordered_set<int> answer;
+  answer.insert(testBoard.getIndex(5, 6));
+  answer.insert(testBoard.getIndex(5, 7));
+  answer.insert(testBoard.getIndex(5, 4));
+  answer.insert(testBoard.getIndex(1, 5));
+  answer.insert(testBoard.getIndex(2, 5));
+  answer.insert(testBoard.getIndex(3, 5));
+  answer.insert(testBoard.getIndex(4, 5));
+  answer.insert(testBoard.getIndex(6, 5));
+  answer.insert(testBoard.getIndex(7, 5));
+
+  EXPECT_TRUE(answer == moves);
+}
+
+// Rook's valid moves when in the corner
+TEST(RookTests, AllValidMovesTest2) {
+  Board testBoard = Board();
+  testBoard.setPiece(0, 0, new Rook(TeamColors::WHITE));
+
+  std::unordered_set<int> moves = testBoard.getPiece(0, 0)->getAllValidMoves(testBoard, 0, 0);
+  std::unordered_set<int> answer;
+  answer.insert(testBoard.getIndex(1, 0));
+  answer.insert(testBoard.getIndex(2, 0));
+  answer.insert(testBoard.getIndex(3, 0));
+  answer.insert(testBoard.getIndex(4, 0));
+  answer.insert(testBoard.getIndex(5, 0));
+  answer.insert(testBoard.getIndex(6, 0));
+  answer.insert(testBoard.getIndex(7, 0));
+  answer.insert(testBoard.getIndex(0, 1));
+  answer.insert(testBoard.getIndex(0, 2));
+  answer.insert(testBoard.getIndex(0, 3));
+  answer.insert(testBoard.getIndex(0, 4));
+  answer.insert(testBoard.getIndex(0, 5));
+  answer.insert(testBoard.getIndex(0, 6));
+  answer.insert(testBoard.getIndex(0, 7));
+
+  EXPECT_TRUE(answer == moves);
+}
+
+TEST(RookTests, OperatorTest1) {
+  Rook rook1 = Rook(TeamColors::WHITE);
+  Rook rook2 = Rook(TeamColors::WHITE);
+  Rook rook3 = Rook(TeamColors::BLACK);
+
+  EXPECT_TRUE(rook1 == rook1);
+  EXPECT_TRUE(rook1 == rook2);
+  EXPECT_TRUE(rook2 != rook3);
+  EXPECT_FALSE(rook2 == rook3);
+}
+
+TEST(RookTests, OperatorTest2) {
+  Rook rook = Rook(TeamColors::WHITE);
+  ChessPiece piece = ChessPiece(TeamColors::WHITE);
+
+  EXPECT_TRUE(rook != piece);
+  EXPECT_FALSE(rook == piece);
+}
+
+
